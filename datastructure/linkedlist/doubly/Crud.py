75e9d9619fae8f61811e75b21d6472ad9d657df9
Ragupathi Kumar <rk819b@att.com>
2022-11-04 09:45:59 +0530

    20221103 :: doubly linkedlist

--- datastructure/linkedlist/doubly/Crud.py
+++ datastructure/linkedlist/doubly/Crud.py
@@ -67,11 +67,78 @@ def add_after(head_node, value, after_node_value):
         node.prev = current
 
 
+def remove_first(head_node):
+
+    if head_node is None or head_node.next is None:
+        return None
+
+    next_node = head_node.next
+    next_node.prev = None
+    return next_node
+
+
+def remove_last(head_node):
+
+    if head_node is None or head_node.next is None:
+        return None
+
+    last_node = head_node
+    while last_node.next is not None:
+        last_node = last_node.next
+
+    last_node.prev.next = None
+    last_node.prev = None
+    return head_node
+
+
+def remove(head_node, value):
+
+    current = head_node
+    while current is not None and current.value != value:
+        current = current.next
+
+    if current is not None:
+
+        if current.prev is not None:
+            current.prev.next = current.next
+        else:
+            head_node = current.next
+
+        if current.next is not None:
+            current.next.prev = current.prev
+
+        current.prev = None
+        current.next = None
+
+    return head_node
+
+
 if __name__ == "__main__":
     head = get_default()
+    read(head)
 
     head = add_first(head, 5)
+    read(head)
+
     head = add_last(head, 6)
+    read(head)
+
     add_after(head, 7, 4)
     read(head)
+
+    head = remove_first(head)
+    read(head)
+
+    head = remove_last(head)
+    read(head)
+
+    head = remove(head, 1)
+    read(head)
+
+    head = remove(head, 7)
+    read(head)
+
+    head = remove(head, 3)
+    read(head)
+
     read_reverse(head)
